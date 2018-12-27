45b417184b56ac8f5ea73a73919bb5f3a49f997e
AmitHasanShuvo <kaziamithasan89@gmail.com>
2018-12-28 02:47:58 +0600

    After adding watermelon2 solution

---
+++ Watermelon2.py
@@ -0,0 +1,16 @@
+'''
+
+Author : kazi_amit_hasan
+Problem: Watermelon
+
+Solution: w must have to be greater than 2 and it should be even number
+'''
+
+
+
+w = int(input())
+
+if(w%2 == 0 and w>2):
+    print("YES")
+else:
+    print("NO")
