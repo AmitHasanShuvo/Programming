dbdb369cd3ebbeca96f7d13b25309abede7a98f9
AmitHasanShuvo <kaziamithasan89@gmail.com>
2019-05-27 02:11:00 +0600

    Merge two binary trees added

---
+++ leetcode617.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeTrees(self, t1, t2):
+
+        if t1 and t2:
+            root = TreeNode(t1.val + t2.val)
+            root.left = self.mergeTrees(t1.left, t2.left)
+            root.right = self.mergeTrees(t1.right, t2.right)
+            return root
+        else:
+            return t1 or t2
