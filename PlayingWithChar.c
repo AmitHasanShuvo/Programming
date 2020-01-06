186743c9bc3c1547fcfe36f93bb03e2f22777358
AmitHasanShuvo <kaziamithasan89@gmail.com>
2020-01-06 07:04:29 +0600

    add

---
+++ PlayingWithChar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+   char ch;
+   char s[20], sen[100];
+
+   scanf("%c", &ch);
+   scanf("%s", s);
+    scanf("\n");
+
+   scanf("%[^\n]%*c", sen);
+
+   printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+
+
+
+
+   return 0;
+
+}
